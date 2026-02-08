2ce99f67f453544b4826f07368c4c0f6139b85f5
chandrakantsahu <chandrakantiiitb@gmail.com>
2026-02-09 03:31:49 +0530

    gh-pages

--- vite.config.js
+++ vite.config.js
@@ -3,7 +3,7 @@ import react from '@vitejs/plugin-react'
 
 // For GitHub Pages: if your site is at https://<username>.github.io/<repo>/,
 // set base to `'/<repo>/'`. For user site https://<username>.github.io use `'/'`.
-const base = process.env.GITHUB_PAGES === 'true' ? '/swe-hq/' : '/'
+const base = process.env.GITHUB_PAGES === 'true' ? '/SWE-HQ/' : '/'
 
 export default defineConfig({
   plugins: [react()],
